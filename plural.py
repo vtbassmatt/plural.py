3708322cea9616e07fbe471b3ec5f67bc4833006
Matt Cooper <github.20.vtbassmatt@spamgourmet.com>
2011-07-09 16:16:08 -0700

    Rules are now compiled from string-based definitions

--- plural.py
+++ plural.py
@@ -12,6 +12,40 @@ def pluralize(wordlist, count, rule):
     """
     return wordlist[index(count, rule)]
     
+rule_definition = {
+    # rule 0: Asian, only one form
+    0: ("True", ),
+    
+    # rule 1: Germanic/English 2-forms (singular, plural)
+    1: ("count == 1", "True"),
+    
+    # rule 2: Romance (0/1 singular, plural)
+    2: ("count == 0 or count == 1", "True"),
+    
+    # rule 3: Latvian (0, ends in 1 other than 11, everything else)
+    3: ("count == 0",
+        "endsin1(count) and count != 11",
+        "True"),
+    
+    # rule 4: Scottish Gaelic (1/11, 2/12, 3-19, everything else)
+    4: ("count == 1 or count == 11",
+        "count == 2 or count == 12",
+        "count >= 3 and count <= 19",
+        "True"),
+}
+
+def createrulefuncs():
+    global rule_definition
+    rulefuncs = []
+    for rule in rule_definition:
+        #print "rule", rule
+        rulefuncs.insert(rule, [])
+        for str in rule_definition[rule]:
+            str = "lambda count: " + str + "\n"
+            #print "::", str
+            rulefuncs[rule].append(eval(compile(str, '<string>', 'eval')))
+    return tuple(rulefuncs)
+
 def expects(rule):
     """
     Returns a count and description of the expected word list for a rule.
@@ -36,41 +70,18 @@ def expects(rule):
     if rule == 4:
         return ("is 1 or 11", "is 2 or 12", "others between 3 and 19", "everything else")
 
-def rulefunctions(rule):
+def getrules(rule, rule_funcs = createrulefuncs()):
     """
     Returns a tuple of functions which can be used to test a count and return the index of the correct word form
     """
-    # rule 0: only one form
-    if rule == 0:
-        return (lambda count: True,)
-        
-    # rule 1: Germanic/English 2-forms (singular, plural)
-    if rule == 1:
-        return (lambda count: count == 1, lambda count: True)
-        
-    # rule 2: Romance (0/1 singular, plural)
-    if rule == 2:
-        return (lambda count: count == 0 or count == 1, lambda count: True)
-        
-    # rule 3: Latvian (0, ends in 1 other than 11, everything else)
-    if rule == 3:
-        return (lambda count: count == 0, lambda count: endsin1(count) and count != 11, lambda count: True)
-        
-    # rule 4: Scottish Gaelic (1/11, 2/12, 3-19, everything else)
-    if rule == 4:
-        return (lambda count: count == 1 or count == 11,
-                lambda count: count == 2 or count == 12,
-                lambda count: count >= 3 and count <= 19,
-                lambda count: True)
-
-def lc(body):
-    return lambda count: body
+    return rule_funcs[rule]
+    
 
 def index(count, rule):
     """
     Determines the index into a wordlist for a particular count and rule.
     """
-    ruleset = rulefunctions(rule)
+    ruleset = getrules(rule)
     for idx in range(len(ruleset)):
         if ruleset[idx](count):
             return idx
