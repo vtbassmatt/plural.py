041fbc048bbce471c78bfabe47a0ab97b75c16e3
Matt Cooper <github.20.vtbassmatt@spamgourmet.com>
2011-07-10 21:24:42 -0700

    Added pluralize() service

--- pluralservice.py
+++ pluralservice.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, json
 from plural import pluralize, explain, rulefor, RuleError
 
 app = Flask(__name__)
@@ -18,6 +18,26 @@ def explain_service(rule):
         explanation = 'null'
         translations = 0
     return '{{"rule":{0}, "translations_required":{2}, "explanation":{1} }}'.format(rule, explanation, translations)
+
+@app.route('/pluralize')
+def pluralize_service():
+    wordlist = request.args.getlist('w')
+    count = int(request.args['c'])
+    rule = int(request.args['r'])
+    
+    response = {"count":count, "rule":rule}
+    
+    try:
+        pluralword = pluralize(wordlist, count, rule)
+        response["word"] = pluralword
+    except IndexError:
+        response["word"] = None
+        response["error"] = "Not enough words passed in"
+    except RuleError:
+        response["word"] = None
+        response["error"] = "Invalid rule specified"
+        
+    return json.dumps(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
