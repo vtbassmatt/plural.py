9c202e5bd2b2c6eab39f269074d1e9aea1eaef55
Matt Cooper <github.20.vtbassmatt@spamgourmet.com>
2011-07-10 20:58:09 -0700

    Added explain() service

--- pluralservice.py
+++ pluralservice.py
@@ -9,5 +9,15 @@ def rulefor_service(langcode):
     rule = rulefor(langcode) or 'null'
     return '{{"langcode":"{0}", "rule":{1} }}'.format(langcode, rule)
 
+@app.route('/rule/<int:rule>')
+def explain_service(rule):
+    try:
+        explanation = explain(rule)
+        translations = len(explanation)
+    except RuleError:
+        explanation = 'null'
+        translations = 0
+    return '{{"rule":{0}, "translations_required":{2}, "explanation":{1} }}'.format(rule, explanation, translations)
+
 if __name__ == '__main__':
     app.run(debug=True)
